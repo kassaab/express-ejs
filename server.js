e5bf2aa3acc63670f8aa85c83089a074a2fe8fe1
kassaab <90882610+kassaab@users.noreply.github.com>
2025-06-18 17:38:51 +0000

    adds edit capability w11

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ const port = 3000;
 //create instance of Express app
 const app = express();
 
+//create session data
+const session = require('express-session');
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: true }));
+app.use(express.urlencoded({ extended: true }));
+
+//pass session data to routes
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
+
 //ejs is a templating engine
 app.set('view engine','ejs');
 
